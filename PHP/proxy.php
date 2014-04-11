c35c4acea5648903f2436fb54af1f2527e68d6c1
phpmaps <dcarroll@esri.com>
2014-04-11 01:17:03 -0700

    Clean gzip, cURL errors, proxy verification

--- PHP/proxy.php
+++ PHP/proxy.php
@@ -331,7 +331,11 @@ class Proxy {
 
         $configError = array(
                 "error" => array("code" => 403,
+<<<<<<< HEAD
                         "details" => array("The proxy tried to resolve a URL that was not found in the configuration file.  Possible solution would be to add another serverUrl into the configuration file or look for typos in the configuration file."),
+=======
+                        "details" => array("The proxy tried to resolve a url that was not found in the configuration file.  Possible solution would be to add another serverUrl into the configuration file or look for typos in the configuration file."),
+>>>>>>> Better headers, no more gzip issues Fixes 65
                         "message" => "Proxy failed due to configuration error."
                 ));
 
