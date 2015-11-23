17b6585cb47ad12beff81850bc9b562864ea82fd
scharfenberg <scharfenberg@TES-Carsten.te.francotyp.com>
2015-11-23 13:40:05 +0100

    playing around: still

--- WordpressPlugin/FpOnlineRateTable/src/Plugin/templates/admin.php
+++
@@ -1,9 +0,0 @@
-<p>
-    <label for="<?php echo $widgetSettings->serviceUrlHtmlId(); ?>">
-        <?php _ex('OnlineRateTable Service Url', 'Widget Settings', 'FpOnlineRateTable'); ?>
-    </label>
-    <input class="widefat" type="url"
-           id="<?php echo $widgetSettings->serviceUrlHtmlId(); ?>"
-           name="<?php echo $widgetSettings->serviceUrlHtmlName(); ?>"
-           value="<?php echo $widgetSettings->serviceUrl(); ?>" />
-</p>
--- WordpressPlugin/FpOnlineRateTable/src/Plugin/templates/widget.php
+++
@@ -1,5 +0,0 @@
-<div class="onlineRateCalculator" ng-init='config = <?php echo $widgetSettings->toJson(); ?>'>
-    <div class="view-container">
-        <div ng-view class="view-frame"></div>
-    </div>
-</div>
