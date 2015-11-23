18351b27b5ce8f23a6c96601e3ffd743698ae0d6
scharfenberg <scharfenberg@TES-Carsten.te.francotyp.com>
2015-11-23 13:32:33 +0100

    for some reason the Netbeans Git Plugin always deletes the files in src/Plugin/Templates. So the changes need to be commited in an additional step

--- WordpressPlugin/FpOnlineRateTable/src/Plugin/Templates/admin.php
+++ WordpressPlugin/FpOnlineRateTable/src/Plugin/Templates/admin.php
@@ -18,7 +18,7 @@ use FP\Web\Portal\FpOnlineRateTable\src\Plugin\Widget\WidgetSettings;
             <p>
                 <?php _ex('Error during contacting RateCalculation service. Please check the settings below and reload the page.',
                         'Widget Setting Error', 'FpOnlineRateTable'); ?><br>
-                <?php $widgetSettings->getgetServiceError(); ?>
+                <?php $widgetSettings->getServiceError(); ?>
             </p>
         </div>
     <?php } else { ?>
@@ -30,7 +30,8 @@ use FP\Web\Portal\FpOnlineRateTable\src\Plugin\Widget\WidgetSettings;
                 name="<?php echo $widgetSettings->htmlName(WidgetSettings::CURRENT_RATE_TABLE_CULTURE); ?>"
                 value="<?php echo $widgetSettings->get(WidgetSettings::CURRENT_RATE_TABLE_CULTURE); ?>" >
             <?php foreach($widgetSettings->getRateTableChoices() as $value => $content) { ?>
-                <option value="<?php echo $value; ?>">
+                <option value="<?php echo $value; ?>"
+                        <?php selected($widgetSettings->getCurrentCulture()->get(), $value); ?>>
                     <?php echo $content; ?>
                 </option>
             <?php } ?>
@@ -39,61 +40,11 @@ use FP\Web\Portal\FpOnlineRateTable\src\Plugin\Widget\WidgetSettings;
 </span>
 
 <span>
-    <label for="<?php echo $widgetSettings->htmlId(WidgetSettings::BASE_SERVICE_URL); ?>">
-        <h4><?php _ex('The base Url of the RateCalculator service (i.e. protocol, hostname, port and api path but not resource path)', 'Widget Settings', 'FpOnlineRateTable'); ?></h4>
+    <label for="<?php echo $widgetSettings->htmlId(WidgetSettings::RESOURCE_URL); ?>">
+        <h4><?php _ex('The base Url of the RateCalculator service (i.e. protocol, hostname, port and resource path but not the method path)', 'Widget Settings', 'FpOnlineRateTable'); ?></h4>
     </label>
     <input class="widefat" type="url"
-           id="<?php echo $widgetSettings->htmlId(WidgetSettings::BASE_SERVICE_URL); ?>"
-           name="<?php echo $widgetSettings->htmlName(WidgetSettings::BASE_SERVICE_URL); ?>"
-           value="<?php echo $widgetSettings->get(WidgetSettings::BASE_SERVICE_URL); ?>" />
-</span>
-
-<span>
-    <label for="<?php echo $widgetSettings->htmlId(WidgetSettings::GET_ACTIVE_RATETABLES_PATH); ?>">
-        <h4><?php _ex('The path of GetActiveTables resource', 'Widget Settings', 'FpOnlineRateTable'); ?></h4>
-    </label>
-    <input class="widefat" type="url"
-           id="<?php echo $widgetSettings->htmlId(WidgetSettings::GET_ACTIVE_RATETABLES_PATH); ?>"
-           name="<?php echo $widgetSettings->htmlName(WidgetSettings::GET_ACTIVE_RATETABLES_PATH); ?>"
-           value="<?php echo $widgetSettings->get(WidgetSettings::GET_ACTIVE_RATETABLES_PATH); ?>" />
-</span>
-
-<span>
-    <label for="<?php echo $widgetSettings->htmlId(WidgetSettings::RATE_CALCULATION_START_PATH); ?>">
-        <h4><?php _ex('The path of RateCalculationStart resource', 'Widget Settings', 'FpOnlineRateTable'); ?></h4>
-    </label>
-    <input class="widefat" type="url"
-           id="<?php echo $widgetSettings->htmlId(WidgetSettings::RATE_CALCULATION_START_PATH); ?>"
-           name="<?php echo $widgetSettings->htmlName(WidgetSettings::RATE_CALCULATION_START_PATH); ?>"
-           value="<?php echo $widgetSettings->get(WidgetSettings::RATE_CALCULATION_START_PATH); ?>" />
-</span>
-
-<span>
-    <label for="<?php echo $widgetSettings->htmlId(WidgetSettings::RATE_CALCULATION_CALCULATE_PATH); ?>">
-        <h4><?php _ex('The path of RateCalculationCalculate resource', 'Widget Settings', 'FpOnlineRateTable'); ?></h4>
-    </label>
-    <input class="widefat" type="url"
-           id="<?php echo $widgetSettings->htmlId(WidgetSettings::RATE_CALCULATION_CALCULATE_PATH); ?>"
-           name="<?php echo $widgetSettings->htmlName(WidgetSettings::RATE_CALCULATION_CALCULATE_PATH); ?>"
-           value="<?php echo $widgetSettings->get(WidgetSettings::RATE_CALCULATION_CALCULATE_PATH); ?>" />
-</span>
-
-<span>
-    <label for="<?php echo $widgetSettings->htmlId(WidgetSettings::RATE_CALCULATION_BACK_PATH); ?>">
-        <h4><?php _ex('The path of RateCalculationBack resource', 'Widget Settings', 'FpOnlineRateTable'); ?></h4>
-    </label>
-    <input class="widefat" type="url"
-           id="<?php echo $widgetSettings->htmlId(WidgetSettings::RATE_CALCULATION_BACK_PATH); ?>"
-           name="<?php echo $widgetSettings->htmlName(WidgetSettings::RATE_CALCULATION_BACK_PATH); ?>"
-           value="<?php echo $widgetSettings->get(WidgetSettings::RATE_CALCULATION_BACK_PATH); ?>" />
-</span>
-
-<span>
-    <label for="<?php echo $widgetSettings->htmlId(WidgetSettings::RATE_CALCULATION_UPDATE_WEIGHT_PATH); ?>">
-        <h4><?php _ex('The path of RateCalculationUpdateWeight resource', 'Widget Settings', 'FpOnlineRateTable'); ?></h4>
-    </label>
-    <input class="widefat" type="url"
-           id="<?php echo $widgetSettings->htmlId(WidgetSettings::RATE_CALCULATION_UPDATE_WEIGHT_PATH); ?>"
-           name="<?php echo $widgetSettings->htmlName(WidgetSettings::RATE_CALCULATION_UPDATE_WEIGHT_PATH); ?>"
-           value="<?php echo $widgetSettings->get(WidgetSettings::RATE_CALCULATION_UPDATE_WEIGHT_PATH); ?>" />
+           id="<?php echo $widgetSettings->htmlId(WidgetSettings::RESOURCE_URL); ?>"
+           name="<?php echo $widgetSettings->htmlName(WidgetSettings::RESOURCE_URL); ?>"
+           value="<?php echo $widgetSettings->get(WidgetSettings::RESOURCE_URL); ?>" />
 </span>
